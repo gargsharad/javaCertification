8db877aa255e6640f489a657a58748b7f0230c2c
gargsharad <garg.sharadkumar@gmail.com>
2014-05-03 17:26:53 -0700

    initial commit

---
+++ src/main/java/com/sharad/ocajp/App.java
@@ -0,0 +1,13 @@
+package com.sharad.ocajp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
